25165acb01ec9c6e0e5138b4a3b1e801b1d5c93a
Pavel Zhuraulevich <p.zhuravlevich@gmail.com>
2024-11-03 00:38:53 +0100

    Add images of items to UI

--- FamilyIslandHelper.Web/wwwroot/js/site.js
+++ FamilyIslandHelper.Web/wwwroot/js/site.js
@@ -1,57 +1,49 @@
 ﻿var BuildingName1 = document.getElementById("BuildingName1");
 var BuildingName2 = document.getElementById("BuildingName2");
 var BuildingName = document.getElementById("BuildingName");
+var ItemCount = document.getElementById("ItemCount");
+var ShowListOfComponents = document.getElementById("ShowListOfComponents");
 
 if (BuildingName1 != null) {
 	BuildingName1.addEventListener("change", function () {
-		updateItems("BuildingName1", "ItemName1");
+		document.getElementById('mainForm').submit();
 	});
 }
 
 if (BuildingName2 != null) {
 	BuildingName2.addEventListener("change", function () {
-		updateItems("BuildingName2", "ItemName2");
+		document.getElementById('mainForm').submit();
 	});
 }
 
 if (BuildingName != null) {
 	BuildingName.addEventListener("change", function () {
-		updateItems("BuildingName", "ItemName");
+		document.getElementById('mainForm').submit();
 	});
 }
 
-var allBuildings = {
-	"CarpentryWorkshop": ["Needle", "Stairs", "Crest", "Stool", "Paints", "Pipe", "Tambourine", "Barrel", "WoodenBeam", "LeatherBall", "Incense"],
-	"JewelryWorkshop": ["SapphireBracelet", "GemNecklace", "AmethystPendant", "EmeraldRing", "PearlEarrings", "CrystalLotus"],
-	"Knocker": ["StoneBlock", "LimestoneBlock", "Beams", "PalmBeams", "Millstone"],
-	"Loom": ["Lace", "Wattle", "Rope", "Gloves", "Sackcloth", "Cloth", "Necklace", "PicnicBasket", "WickerBasket", "DreamCatcher", "DyedFabric"],
-	"MeteoriteForge": ["IronIngot", "IronPipe", "IronPlate", "Hammer"],
-	"Mill": ["GoatFood", "ChickenFood", "Ocher", "Flour", "SunflowerOil", "Syrup", "CowFood"],
-	"Mixer": ["Soap", "Butter", "Cheese", "BluePaint", "LemonOil", "WhippedCream"],
-	"Pottery": ["Bowl", "Potp", "Jugp", "Amphorap", "Flashlight"],
-	"Sawmill": ["Stakes", "UnedgedBoard", "EdgedBoard", "Trough"],
-	"ShamanWorkshop": ["RuneStone", "FlowerWreath", "FragrantBouquet"],
-	"Smelter": ["Resin", "Coal", "Gold", "Shingles", "BurntBrick", "Nails"],
-	"Tannery": ["Leather", "Papyrus", "WhitePaint"],
-	"Workshop": ["Scraper", "Axe", "Knife", "Brick"]
-};
+if (ItemCount != null) {
+	ItemCount.addEventListener("change", function () {
+		document.getElementById('mainForm').submit();
+	});
+}
 
-function updateItems(buildingNameId, itemNameId) {
-	var itemName = document.getElementById(itemNameId);
-	let selectedBuildingName = document.getElementById(buildingNameId);
+if (ShowListOfComponents != null) {
+	ShowListOfComponents.addEventListener("change", function () {
+		document.getElementById('mainForm').submit();
+	});
+}
 
-	while (itemName.options.length > 0) {
-		itemName.remove(itemName.options.length - 1);
+function setItemName(event) {
+	document.getElementById('ItemName').value = event.currentTarget.id;
+
+	document.getElementById('mainForm').submit();
+
+	var itemNameImages = document.getElementsByClassName('itemNameImage');
+
+	for (let i = 0; i < itemNameImages.length; i++) {
+		itemNameImages[i].style.border = "none grey";
 	}
 
-	var building = allBuildings[selectedBuildingName.value];
-
-	for (i = 0; i < building.length; i++) {
-		var opt = document.createElement('option');
-
-		opt.text = building[i];
-		opt.value = building[i];
-
-		itemName.add(opt, null);
-	}
+	event.currentTarget.style.border = "solid grey";
 }
