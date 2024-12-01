0ef603135a00c4732fed409b2235fed2ed008b44
Pavel Zhuraulevich <p.zhuravlevich@gmail.com>
2024-12-01 15:26:03 +0100

    desktop: Add ability to choice API version

---
+++ FamilyIslandHelper.Api/ApiVersion.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace FamilyIslandHelper.Api
+{
+	public enum ApiVersion
+	{
+		[Description("API before update 10.2024")]
+		v1,
+		[Description("API after update 10.2024")]
+		v2
+	}
+}
