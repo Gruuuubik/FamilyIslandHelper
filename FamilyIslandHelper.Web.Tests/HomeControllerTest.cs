23bd23511a052e8b95e54c4ca203fcd36dc4867c
Pavel Zhuraulevich <p.zhuravlevich@gmail.com>
2024-12-25 23:00:57 +0100

    web: Add tree view for components for page Home

--- FamilyIslandHelper.Web.Tests/HomeControllerTest.cs
+++ FamilyIslandHelper.Web.Tests/HomeControllerTest.cs
@@ -26,7 +26,8 @@ namespace FamilyIslandHelper.Web.Tests
 				3.1. Check that building is selected.
 				3.2. Check that the 1st item from this building is selected.
 				3.3. Check item info.
-				3.4. Check that another item from this building is not selected.
+				3.4. Check total time info
+				3.5. Check that another item from this building is not selected.
 			4. Select Item 2 from current building.
 				4.1. Check that Item 2 from this building is selected.
 				4.2. Check item info.
@@ -35,9 +36,9 @@ namespace FamilyIslandHelper.Web.Tests
 			6. Set ItemCount = 2.
 				6.1. Check that item info is updated.
 		")]
-		[TestCase("ApiVersionHome1", 13, "JewelryWorkshop", "PearlEarrings", "01:15:00, 180", "SapphireBracelet", "01:15:00, 485", "02:30:00, 970", 1)]
-		[TestCase("ApiVersionHome2", 17, "Forge", "Needle", "00:08:00, 216", "Glass", "00:35:00, 1928", "01:10:00, 3856", 2)]
-		public void HomePageTest(string apiVersionId, int buildingsCount, string buildingId, string item1Id, string item1Info, string item2Id, string item2Info, string item1InfoFor2, int testCaseNumber)
+		[TestCase("ApiVersionHome1", 13, "JewelryWorkshop", "PearlEarrings", "01:15:00, 180", "03:15:00", "SapphireBracelet", "01:15:00, 485", 25, "Sackcloth", "Cloth", "02:30:00, 970", 1)]
+		[TestCase("ApiVersionHome2", 17, "Forge", "Needle", "00:08:00, 216", "00:08:00", "Glass", "00:35:00, 1928", 8, "Grass", "Axe", "01:10:00, 3856", 2)]
+		public void HomePageTest(string apiVersionId, int buildingsCount, string buildingId, string item1Id, string item1Info, string totalTimeInfo1, string item2Id, string item2Info, int expectedComponentsTreeImagesCount, string componentsTreeItemComponentImageId, string componentsTreeItemImageId, string item1InfoFor2, int testCaseNumber)
 		{
 			try
 			{
@@ -59,6 +60,9 @@ namespace FamilyIslandHelper.Web.Tests
 				var itemInfo = driver.FindElement(By.Id(ItemInfoId));
 				Assert.That(itemInfo.Text, Does.Contain(item1Info));
 
+				var totalTimeInfo = driver.FindElement(By.Id("totalTimeInfo"));
+				Assert.That(totalTimeInfo.Text, Is.EqualTo(totalTimeInfo1));
+
 				var item2 = driver.FindElement(By.Id(item2Id));
 				Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage"));
 				item2.Click();
@@ -71,6 +75,15 @@ namespace FamilyIslandHelper.Web.Tests
 				driver.FindElement(By.Id("ShowListOfComponents")).Click();
 				itemInfo = driver.FindElement(By.Id(ItemInfoId));
 				Assert.That(itemInfo.Text, Does.Contain("Components"));
+
+				var componentsTree = driver.FindElement(By.Id("componentsTree"));
+				var componentsTreeImages = componentsTree.FindElements(By.TagName("img"));
+				Assert.That(componentsTreeImages, Has.Count.EqualTo(expectedComponentsTreeImagesCount));
+
+				var grass = driver.FindElement(By.Id(componentsTreeItemComponentImageId));
+				Assert.That(grass.Displayed, Is.True);
+				driver.FindElement(By.Id(componentsTreeItemImageId)).Click();
+				Assert.That(grass.Displayed, Is.False);
 
 				var itemCount = driver.FindElement(By.Id("ItemCount"));
 				new Actions(driver).MoveToElement(itemCount).Click().SendKeys(Keys.Backspace).SendKeys("2").SendKeys(Keys.Enter).Perform();
