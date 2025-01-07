781bdd10ffe18fc5bd3d88b24b6c93b30b2fc822
Pavel Zhuraulevich <p.zhuravlevich@gmail.com>
2025-01-07 01:27:27 +0100

    api: Add StoneTile to Knocker

--- FamilyIslandHelper.Api/Models/Items_v2/KnockerItems.cs
+++ FamilyIslandHelper.Api/Models/Items_v2/KnockerItems.cs
@@ -10,13 +10,28 @@ namespace FamilyIslandHelper.Api.Models.Items_v2
 	{
 		public override string Name => "Каменный блок";
 		public override int LevelWhenAppears => 30;
-		public override TimeSpan OriginalProduceTime => TimeSpan.FromHours(1);
+		public override TimeSpan OriginalProduceTime => TimeSpan.FromMinutes(50);
 		public override Building BuildingToCreate => new Knocker();
 
 		public override List<(Item item, int count)> Components => new List<(Item item, int count)>
 			{
 				(new Stone(), 15),
 				(new Hammer(), 2)
+			};
+	}
+
+	public class StoneTile : ProducibleItem
+	{
+		public override string Name => "Каменная плитка";
+		public override int LevelWhenAppears => 70;
+		public override TimeSpan OriginalProduceTime => TimeSpan.FromHours(1);
+		public override Building BuildingToCreate => new Knocker();
+
+		public override List<(Item item, int count)> Components => new List<(Item item, int count)>
+			{
+				(new Brick(), 2),
+				(new Nails(), 3),
+				(new Stakes(), 6)
 			};
 	}
 }
